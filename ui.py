1b0d2bb285afce10459e38611399f5f4c5595e59
Mohamed Kamal <mkrashed1999@gmail.com>
2025-12-15 01:11:10 +0200

    test
    test

---
+++ ui.py
@@ -0,0 +1,26 @@
+import pygame
+from config import Colors
+
+
+class Button:
+    def __init__(self, rect: pygame.Rect, text: str, font: pygame.font.Font, onclick=None):
+        self.rect = rect
+        self.text = text
+        self.font = font
+        self.onclick = onclick
+        self.hover = False
+
+    def update_hover(self, mouse_pos):
+        self.hover = self.rect.collidepoint(mouse_pos)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.hover and self.onclick:
+                self.onclick()
+
+    def draw(self, surf):
+        color = Colors.BUTTON_HOVER if self.hover else Colors.BUTTON
+        pygame.draw.rect(surf, color, self.rect, border_radius=10)
+
+        txt = self.font.render(self.text, True, Colors.TEXT)
+        surf.blit(txt, txt.get_rect(center=self.rect.center))
